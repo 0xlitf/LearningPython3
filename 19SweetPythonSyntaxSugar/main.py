79e66320d3eea1aae1df5ef245bbc4cd17573b10
0xlitf <0xlitf@gmail.com>
2023-12-26 11:09:32 +0800

    `

--- 19SweetPythonSyntaxSugar/main.py
+++ 19SweetPythonSyntaxSugar/main.py
@@ -3,7 +3,7 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     # ...
-#     sys.exit(app.exec())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    # ...
+    sys.exit(app.exec())
